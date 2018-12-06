8b73af20274734bff809d2eacc1bdf1939b416f3
yiiu <yu.gintoki@gmail.com>
2018-12-06 16:08:58 +0800

    update script & package lock

--- packages/radish/lib/config/webpack/loader/script.ts
+++ packages/radish/lib/config/webpack/loader/script.ts
@@ -1,3 +1,7 @@
+import * as path from 'path';
+
+import paths from '../../paths';
+
 export interface IScriptLoaderConfig {
   isServer: boolean;
 }
@@ -7,24 +11,46 @@ export default (_: IScriptLoaderConfig) => {
     test: /\.(js|mjs|jsx|ts|tsx)$/,
     exclude: /node_modules/,
     use: [
-      // require.resolve('cache-loader'),
+      {
+        loader: require.resolve('cache-loader'),
+        options: {
+          cacheDirectory: path.join(paths.appNodeModules, 'node_modules/.cache/babel-loader')
+        }
+      },
       {
         loader: require.resolve('babel-loader'),
         options: {
+          babelrc: false,
+          configFile: false,
+          cacheDirectory: true,
+          presets: [
+            [
+              require.resolve('@babel/preset-typescript'),
+              {
+                isTSX: true,
+                allExtensions: true
+              }
+            ],
+            require.resolve('@babel/preset-env'),
+            require.resolve('@babel/preset-react'),
+          ],
           plugins: [
-            require.resolve('react-hot-loader/babel'),
+            require.resolve('@babel/plugin-transform-runtime'),
             require.resolve('@babel/plugin-syntax-dynamic-import'),
             require.resolve('@babel/plugin-proposal-object-rest-spread'),
-            require.resolve('@babel/plugin-proposal-class-properties'),
-            [require.resolve('@babel/plugin-transform-runtime'), {
-              corejs: 2,
-            }],
-            require.resolve('react-loadable/babel'),
+            [
+              require.resolve('@babel/plugin-proposal-decorators'),
+              {
+                legacy: true
+              }
+            ],
+            [require.resolve('@babel/plugin-proposal-class-properties'), { loose: true }],
+            require.resolve('@babel/plugin-proposal-json-strings'),
+            require.resolve('@babel/plugin-proposal-function-bind'),
+            require.resolve('react-hot-loader/babel'),
+            require.resolve('react-loadable/babel')
           ],
         },
-      },
-      {
-        loader: require.resolve('ts-loader'),
       },
     ],
   };
